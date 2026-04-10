a9d6092c9f8e763aa732b22757f74a82c78a85d9
nick duit <nick.duit@gmail.com>
2026-04-10 13:58:50 +0200

    bestanden toegevoegd

--- helper.py
+++ helper.py
@@ -11,6 +11,13 @@ def fooi_pp(bedrag, personen):
     except:
         bedrag_pp = "??"
     return f"Het bedrag per persoon is {bedrag_pp} euro"
-b = int(input("Welk bedrag zit er in de fooienpot? "))
-p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
-print(fooi_pp(b,p))
+def onderstreep(tekst=""):
+    uit = []
+    uit.append(tekst)
+    uit.append(len(tekst) * "=")
+    return uit
+def som(d):
+    totaal = 0
+    for waarde in d.values():
+        totaal += waarde
+    return totaal
---
+++ presentatie.py
@@ -0,0 +1,5 @@
+def presenteer(d, totaal):
+    for sleutel, waarde in d.items():
+        print(f"{sleutel} : {waarde}")
+    print("==========================")
+    print(f"totaal : {totaal}")
